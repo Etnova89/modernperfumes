b834d634184abc7d8c9bb65199e5e5978660a013
Eric Terranova <terranova069@gmail.com>
2019-12-14 18:04:24 -0500

    Excel parser working for nandasons excelsheet

--- src/main/java/com/modernperfumes/main/model/Perfume.java
+++ src/main/java/com/modernperfumes/main/model/Perfume.java
@@ -1,12 +1,20 @@
 package com.modernperfumes.main.model;
 
+import java.util.ArrayList;
+
 public class Perfume  {
     int id;
     String description;
     String sku;
-    String price;
+    Double price;
     Integer quantity;
     Integer vendorId;
+    String brand;
+    static String[] priceAliases = new String[] {"price", "special price", "price us$"};
+    static String[] descriptionAliases = new String[] {"description", "long description"};
+    static String[] skuAliases = new String[] {"sku", "upc"};
+    static String[] quantityAliases = new String[] {"quantity", "qty"};
+    static String[] brandAliases = new String[] {"brand"};
 
     public int getId() {
         return id;
@@ -40,11 +48,11 @@ public class Perfume  {
 //        this.price = price;
 //    }
 
-    public String getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
@@ -68,24 +76,81 @@ public class Perfume  {
         this.vendorId = vendorId;
     }
 
-    public void setProperty(String property, String value) {
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setProperty(String property, Object value) {
         switch(property) {
             case "price":
 //                setPrice(Double.valueOf(value));
-                setPrice(value);
+                setPrice((Double) value);
                 break;
             case "sku":
-                setSku(value);
+                setSku((String) value);
                 break;
             case "description":
-                setDescription(value);
+                setDescription((String) value);
                 break;
             case "quantity":
-                setQuantity(Integer.valueOf(value));
+                setQuantity(Integer.valueOf((Integer) value));
+                break;
+            case "brand":
+                setBrand((String) value);
                 break;
             default:
                 break;
                 // code block
         }
     }
+
+    public static boolean isPriceAlias(String priceHeaderName) {
+        for (String alias: priceAliases) {
+            if(priceHeaderName.toLowerCase().contains(alias)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isDescriptionAlias(String descriptionHeaderName) {
+        for (String alias: descriptionAliases) {
+            if(descriptionHeaderName.toLowerCase().contains(alias)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSkuAlias(String skuHeaderName) {
+        for (String alias: skuAliases) {
+            if(skuHeaderName.toLowerCase().contains(alias)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isQuantityAlias(String quantityHeaderName) {
+        for (String alias: quantityAliases) {
+            if(quantityHeaderName.toLowerCase().contains(alias)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBrandAlias(String brandHeaderName) {
+        for (String alias: brandAliases) {
+            if(brandHeaderName.toLowerCase().contains(alias)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/modernperfumes/main/service/SpreadsheetParser.java
+++ src/main/java/com/modernperfumes/main/service/SpreadsheetParser.java
@@ -1,36 +1,60 @@
 package com.modernperfumes.main.service;
 
 import com.modernperfumes.main.model.Perfume;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SpreadsheetParser {
     public List<Perfume> parseExcel() throws IOException {
+        List<Perfume> perfumes = new ArrayList<>();
         String FILE_NAME = "src/main/resources/Nandansons.xls";
         try(InputStream stream = new FileInputStream(FILE_NAME)) {
-//            CSVReader reader = new CSVReader(new InputStreamReader(stream, "UTF-8"));
-//            Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(new InputStreamReader(stream, "UTF-8"));
-//            Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(new InputStreamReader(stream, "UTF-16"));
-//            for (CSVRecord record : records) {
-//            String lastName = record.get("Last Name");
-//            String firstName = record.get("First Name");
+            HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(FILE_NAME));
+            HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
+            Map<Integer, String> columnMapper = new HashMap<Integer, String>();
+            for (int i = 0; i < myExcelSheet.getLastRowNum(); i++) {
+                HSSFRow row = myExcelSheet.getRow(i);
+                Perfume perfume = new Perfume();
+                if(i == 0 ) {
+                    setColumnHeaders(columnMapper, row);
+                } else {
+                    for(int x = 0; x < row.getLastCellNum(); x++) {
+                        if(columnMapper.get(x) != null && !StringUtils.isEmpty(columnMapper.get(x))) {
+                            if(HSSFCell.CELL_TYPE_NUMERIC == row.getCell(x).getCellType()) {
+                                perfume.setProperty(columnMapper.get(x), row.getCell(x).getNumericCellValue());
+                            } else {
+                                if(!StringUtils.isEmpty(row.getCell(x).getStringCellValue())) {
+                                    perfume.setProperty(columnMapper.get(x), row.getCell(x).getStringCellValue());
+                                }
+                            }
+                        }
+                    }
+                    perfumes.add(perfume);
+                }
             }
-        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(FILE_NAME));
-        HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
-        for (int i = 0; i < myExcelSheet.getLastRowNum(); i++) {
-            HSSFRow row = myExcelSheet.getRow(i);
-                System.out.print("hi");
+            return perfumes;
+        }
+    }
 
+    private void setColumnHeaders(Map<Integer, String> columnMapper, HSSFRow row) {
+        for (int r=0; r < row.getLastCellNum(); r++) {
+            if(Perfume.isPriceAlias(row.getCell(r).getStringCellValue())) {
+                columnMapper.put(r, "price");
+            } else if(Perfume.isDescriptionAlias((row.getCell(r).getStringCellValue()))) {
+                columnMapper.put(r, "description");
+            } else if(Perfume.isSkuAlias((row.getCell(r).getStringCellValue()))) {
+                columnMapper.put(r, "sku");
+            } else if(Perfume.isQuantityAlias((row.getCell(r).getStringCellValue()))) {
+                columnMapper.put(r, "quantity");
             }
-            Reader in = new FileReader(FILE_NAME);
-            return null;
+        }
     }
 }
