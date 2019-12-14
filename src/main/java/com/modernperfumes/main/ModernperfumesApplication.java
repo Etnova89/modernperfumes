81d111a1390b00c130f50690d6c4505593c9caf6
Eric Terranova <terranova0689@gmail.com>
2019-12-13 20:00:00 -0500

    Initial commit

---
+++ src/main/java/com/modernperfumes/main/ModernperfumesApplication.java
@@ -0,0 +1,13 @@
+package com.modernperfumes.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernperfumesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernperfumesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modernperfumes/main/ModernperfumesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modernperfumes.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernperfumesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
