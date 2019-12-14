c4cb8ecdfb5633409bc26d4461ffd72c7a1b501c
Eric Terranova <terranova0689@gmail.com>
2019-12-13 22:24:34 -0500

    added csv to resources

---
+++ src/main/java/com/modernperfumes/main/model/PdfStyle.java
@@ -0,0 +1,11 @@
+package com.modernperfumes.main.model;
+
+public class PdfStyle {
+
+    int id;
+    String description;
+    String sku;
+    double price;
+    Integer
+
+}
---
+++ src/main/java/com/modernperfumes/main/model/Vendor.java
@@ -0,0 +1,32 @@
+package com.modernperfumes.main.model;
+
+public class Vendor {
+
+    int id;
+    String name;
+    String styleId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStyleId() {
+        return styleId;
+    }
+
+    public void setStyleId(String styleId) {
+        this.styleId = styleId;
+    }
+}
